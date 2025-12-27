06a69cd48b0711b0d0d2622b7bb153a2755d5d54
Eboukadida <elias.boukadida@epitech.eu>
2025-12-27 15:54:56 +0100

    maj

---
+++ script.js
@@ -0,0 +1,15 @@
+AOS.init({
+    once: true,
+});
+
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+}
+
+window.addEventListener('load', function() {
+    window.scrollTo(0, 0);
+
+    if (window.location.hash) {
+        history.replaceState(null, null, window.location.pathname);
+    }
+});
